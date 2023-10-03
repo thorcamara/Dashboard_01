efbc379d2d8e71708670049472389931c4b50425
thorcamara <thorcamaraalthoff@gmail.com>
2023-10-02 21:24:43 -0300

    Added constants and EventListener

---
+++ script.js
@@ -0,0 +1,9 @@
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
