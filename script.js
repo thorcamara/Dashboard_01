2d8cc97bc29fcfcc4699a69c42c9e164925e0c5d
thorcamara <thorcamaraalthoff@gmail.com>
2023-10-02 21:24:58 -0300

    Added more EventListeners

--- script.js
+++ script.js
@@ -5,5 +5,15 @@ const closeBtn = document.getElementById('close-btn');
 const darkMode = document.querySelector('.dark-mode');
 
 menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
+  sideMenu.style.display = 'block';
 });
+
+closeBtn.addEventListener('click', () => {
+  sideMenu.style.display = 'none';
+});
+
+darkMode.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode-variables');
+  darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+  darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+})
